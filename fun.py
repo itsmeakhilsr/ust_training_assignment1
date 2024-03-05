4ad6535bbb79d20c18520aca0b0ca555a121e3b5
itsmeakhilsr <akhilsr628@gmail.com>
2024-03-05 22:23:24 +0530

    Added function fun(a)

--- fun.py
+++ fun.py
@@ -1,5 +1,11 @@
 def fun(a):
-    b=10/a 
-    return a 
+    try:
+        result = 100 / a 
+        print("this is function 1")
+        print("Result:", result)
+        return a 
+    except ZeroDivisionError:
+        print("Error: Division by zero!")
+        return None
 
-print(fun(0))
+print(fun(30))
